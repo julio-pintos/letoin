b23513a9a41893fbcf4f02a490b4102aa3e00a22
Julio Pintos <julio@tarmac.io>
2015-10-20 16:42:58 -0300

    rescue

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
+require 'oauth/request_proxy/rack_request'
+
+OAUTH_10_SUPPORT = true
 
 # Initialize the rails application
 LtiTest::Application.initialize!
